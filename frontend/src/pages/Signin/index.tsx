12b6b4b69302ff8548284b87074b4b3e84be00b2
Bibi <bibiaremieye@gmail.com>
2021-06-13 20:15:38 +0100

    remove all un use code

--- frontend/src/pages/Signin/index.tsx
+++ frontend/src/pages/Signin/index.tsx
@@ -1,13 +1,9 @@
 import SignInComponent from "components/Auth/SignIn";
-import { AuthContext } from "context/auth/AuthProvider";
-import React, { useContext } from "react";
 import { useHistory } from "react-router";
 import { SigninUserType } from "types/authTypes";
 
 const SignInPage = () => {
   const history = useHistory();
-
-  const { signInUserContext } = useContext(AuthContext);
 
   const onSubmit = async (values: SigninUserType, { setSubmitting, setErrors }: any) => {
     console.log(values);
